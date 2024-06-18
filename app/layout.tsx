1fb9c596fe0488fff6fa59e2a8e02e81a3d1f4fb
Jason Karlavige <jkarlavige@gmail.com>
2024-06-18 14:45:56 -0400

    show draft mode banner when in draft mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
+import { draftMode } from 'next/headers'
+
 export default function RootLayout({ children }) {
+  const isDraftMode = draftMode().isEnabled
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        {isDraftMode ? (
+          <h2 style={{ background: 'red', color: 'white' }}>IS DRAFT MODE</h2>
+        ) : null}
+      </body>
     </html>
   );
 }
